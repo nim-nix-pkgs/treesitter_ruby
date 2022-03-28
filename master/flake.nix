4d5c65ade215136790450b00e226f2bbc2218874
hugosenari <hugosenari@gmail.com>
2022-03-27 21:57:26 -0300

    chore: re index treesitter_ruby

--- master/flake.nix
+++ master/flake.nix
@@ -11,8 +11,8 @@
   inputs.src-treesitter_ruby-master.ref   = "refs/heads/master";
   inputs.src-treesitter_ruby-master.owner = "genotrance";
   inputs.src-treesitter_ruby-master.repo  = "nimtreesitter";
-  inputs.src-treesitter_ruby-master.dir   = "treesitter_ruby";
-  inputs.src-treesitter_ruby-master.type  = "github";
+    inputs.src-treesitter_ruby-master.dir   = "treesitter_ruby";
+inputs.src-treesitter_ruby-master.type  = "github";
   
   inputs."nimgen".owner = "nim-nix-pkgs";
   inputs."nimgen".ref   = "master";
